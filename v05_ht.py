8edcf5c5486e536280107673f4b50f0d94ef72ce
evanti <kalmykov.alik@gmail.com>
2018-04-07 14:57:14 +0300

    new prod

---
+++ v05_ht.py
@@ -0,0 +1,193 @@
+import gc, select, sys, time, socket, selectors, os, subprocess
+from multiprocessing import Process, Manager
+from worker import *
+
+class Task(object):
+	def __init__(self, sock):
+		self.target=worker() # this must be the name of the target function
+		self.status=True
+		self.socket=sock
+		self.stillworking=self.target.send(None) #send False from worker if we're finished, use StopIter exception in worker
+		if self.stillworking:
+			fisrt_conn_res = self.target.send(sock)
+			if fisrt_conn_res[0]=='start':
+				self.first_mask = fisrt_conn_res[1]
+				self.ttime = int(time.time() + fisrt_conn_res[2])
+				return
+		self.close()
+		self.status=False
+		return
+
+	def close(self):
+		self.target.close()
+		self.socket.close()
+
+	def run(self):
+		try:
+			output=self.target.send(None)
+		except StopIteration:
+			self.target.close()
+			output=('fin', 0)
+		return output
+	def __repr__(self):
+		q=('-').join((str(self.target),  str(self.fd), str(self.ttime - int(time.time()))))
+		return q
+
+def showstats(taskdict):
+	global p_success_list
+	bb=[]
+	for i in taskdict:
+		bb.append(taskdict[i].target.gi_frame.f_lineno)
+	cc=set(bb)
+	dd={}
+	for i in cc:
+		dd[i]=bb.count(i)
+	with open("attack_stats.txt", 'a') as fi:
+		print('Total tasks:', len(taskdict), file=fi)
+		for key in sorted(dd):
+			print('%s: %s' % (key, dd[key]), file=fi)
+
+def process(max_work, attack_chunk, attack_timeout, cycles_num):
+	still_working=True
+	def add_new_task():
+		try:
+			s=socket.socket()
+			s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			s.setblocking(False)
+		except OSError:
+			return False
+		a = Task(s)
+		if a.stillworking and a.status:
+			sel.register(s.fileno(), 2)#select.EPOLLOUT | select.EPOLLET)
+			taskmap[s.fileno()]=a
+			return True
+		if not a.stillworking:
+			return 'Exhausted'
+		if not a.status:
+			return 'Retry'
+
+	def ready():
+		ready=sel.select(0)
+		return ready
+
+	''' First the setup '''
+	# sel = select.epoll()
+	sel=selectors.DefaultSelector()
+	taskmap = {}
+	attacklist=[]
+	k=0
+
+	'''Now execution'''
+	while k<cycles_num:
+
+		'''Main loop'''
+		while 1:
+			if not ready() and len(taskmap)<max_work and still_working:
+				res=add_new_task()
+				if res=='Exhausted':
+					still_working=False
+					break
+				elif res=='Retry':
+					break
+			else:
+				if len(taskmap)==0: exit()
+				break
+
+		for key, event in ready():
+			if event not in [1,2]:
+				sel.unregister(key.fd)
+				taskmap[key.fd].target.close()
+				del taskmap[key.fd]
+				continue
+			res = taskmap[key.fd].run()
+			if res[0]=='mask':
+				sel.modify(key.fd, res[1])
+				taskmap[key.fd].ttime=int(time.time()+res[2])
+			# print('Time set', taskmap[key.fd].ttime)
+			elif res[0]=='fin':
+				sel.unregister(key.fd)
+				taskmap[key.fd].close()
+				del taskmap[key.fd]
+				continue
+			elif res[0]=='ready':
+				sel.unregister(key.fd)
+				attacklist.append(res[1])
+				taskmap[key.fd].close()
+				del taskmap[key.fd]
+
+		'''Attack launcher'''
+		if len(attacklist)>=attack_chunk:
+
+			p1 = Process(target=attack, args=(attacklist,))
+			p1.start()
+			showstats(taskmap)
+			print("Attack launched", len(attacklist))
+			p_success_list.append(len(attacklist))
+
+			attacklist.clear()
+
+		'''Gagbage cleaning'''
+		now = int(time.time())
+		del_list=[]
+		for i in taskmap:
+			if taskmap[i].ttime and taskmap[i].ttime < now:
+				sel.unregister(i)
+				taskmap[i].close()
+				del_list.append(i)
+		# print('To delete', len(del_list))
+		for i in del_list:
+			del taskmap[i]
+
+
+def attack(strikelist):
+	res = []
+	for i in strikelist:
+		res.append((':').join((i[0], str(i[1]))))
+	with open('scanres.txt', 'a') as fi:
+		for i in res:
+			fi.write(str(i)+'\n')
+	sys.exit(0)
+
+manager = Manager()
+p_success_list=manager.list()
+eff_list=manager.list()
+
+def mainloop():
+	global p_success_list
+	maximum_workers = 150
+	attack_chunk=1
+	cycles = 50  # before respawning processes
+	conc_proc = 1  # concurrent processes
+	repetitions=1 # NUMBER OF PROCESS RESPAWNS
+
+
+	attack_time_limit = 55
+	starttime = time.time()
+	total_number=0
+	for _ in range(repetitions):
+		process_time=time.time()
+		jobs=[]
+		try:
+			for j in range(conc_proc):
+				p=Process(target=process, args=(maximum_workers, attack_chunk, attack_time_limit, cycles ))
+				jobs.append(p)
+			for p in jobs:
+				p.start()
+			for p in jobs:
+				p.join()
+		except:
+			pass
+		finally:
+			total_time = time.time() - starttime
+			total_number = sum(p_success_list)
+			efficiency = int(total_number / total_time)
+			# eff_list.append((efficiency, attack_number, attack_chunk,conc_proc))
+			print("Total efficiency", int(efficiency), "total number:", total_number, 'time', str(int(total_time)))
+
+
+
+if __name__=='__main__':
+	try:
+		mainloop()
+	except KeyboardInterrupt:
+		gc.collect(2)
---
+++ worker.py
@@ -0,0 +1,77 @@
+import socket, itertools, struct
+from random import shuffle
+print('import worked')
+
+socks51=b'\x05\x01\x00'
+
+def gen_hosts(f_0):
+	a=[x for x in range(1)]
+	shuffle(a)
+	b=[x for x in range(256)]
+	shuffle(b)
+	c=[x for x in range(256)]
+	shuffle(c)
+	ports = [8088, 8888, 3128, 33555, 61177, 8060, 31544, 1080, 8080, 61234, 35618, 53281, 20183, 33012, 39880, 8081, 9999, 65000, 9050]
+	q=itertools.product(a,b,c)
+	while 1:
+		try:
+			zz = str(f_0) + '.' + ('.').join([str(x) for x in next(q)])
+		except StopIteration: break
+		zzz=((zz, i) for i in ports)
+		while 1:
+			try:
+				yield next(zzz)
+			except StopIteration: break
+target=gen_hosts(120)
+
+# counter=0
+# while 1:
+# 	try:
+# 		print(next(target))
+# 		counter+=1
+# 	except StopIteration:
+# 		print(counter)
+# 		break
+
+
+def worker():
+	### INDIVIDUAL PARAMETERS ###
+	try:
+		tartg = next(target)
+		print(tartg)
+		s=yield True
+	except StopIteration:
+		print('out of addresses')
+		yield False
+
+	### INDIVIDUAL PARAMETERS ###
+
+
+	try:
+		s.connect(tartg)
+	except BlockingIOError:
+		pass
+	except Exception as e:
+		print(31, str(e))
+		yield ('fin', 0)
+	yield ('start', 4, 3)
+
+	try:
+		s.sendall(socks51)
+	except Exception as e:
+		print(32, str(e))
+		yield ('fin', 0)
+
+	yield ('mask', 1, 5)
+
+
+	try:
+		s51resp=s.recv(1024)
+	except Exception as e:
+		print(33, str(e))
+		yield ('fin', 0)
+	print('s51 received', s51resp)
+	if len(s51resp)>0:
+		if [x for x in s51resp]==[5,0]:
+			yield ('ready', tartg) # ready socket + message to send
+	yield ('fin', 0)
